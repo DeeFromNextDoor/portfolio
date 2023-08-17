b5df714c33cf84d182f4020356ed9a957c794819
DeeFromNextDoor <simeonvictordayo@gmail.com>
2023-08-17 14:17:56 +0100

    new data added to the data file

--- src/data/Data.jsx
+++ src/data/Data.jsx
@@ -6,7 +6,7 @@ export const Projects = [
     image: "./worldFlags.png",
     technologies: ["React", "HTML", "CSS"],
     liveDemo: "https://example.com/project1",
-    sourceCode: "https://github.com/username/project2",
+    sourceCode: "https://github.com/DeeFromNextDoor/world-flags.git",
   },
   {
     title: "Project 2",
@@ -14,7 +14,7 @@ export const Projects = [
       "With Shopie, you can easily create, edit, and delete items, keep track of quantities and prices, and even add them to your virtual cart for a seamless shopping experience.",
     image: "./shoppie.png",
     technologies: ["JavaScript", "HTML", "CSS"],
-    liveDemo: "https://example.com/project2",
+    liveDemo: "https://shopieapp.netlify.app/",
     sourceCode: "https://github.com/username/project2",
   },
 ];
